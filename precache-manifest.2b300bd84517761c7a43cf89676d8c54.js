87085ba48deaa7b42090492fc8bf820e6976b3d7
ivanbkovacevic <ivanbkovacevic@gmail.com>
2021-04-26 21:14:26 +0200

    Updates

---
+++ precache-manifest.2b300bd84517761c7a43cf89676d8c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e2173d0f09fa82b40fa",
+    "url": "/Puzzle/static/css/main.39818c60.chunk.css"
+  },
+  {
+    "revision": "3e2173d0f09fa82b40fa",
+    "url": "/Puzzle/static/js/main.3e2173d0.chunk.js"
+  },
+  {
+    "revision": "8df21f5eefc4fba9b00e",
+    "url": "/Puzzle/static/js/runtime~main.8df21f5e.js"
+  },
+  {
+    "revision": "d17a86313a39f0060049",
+    "url": "/Puzzle/static/js/2.d17a8631.chunk.js"
+  },
+  {
+    "revision": "8d3a9594a705d8f2cfbce4a347f00de0",
+    "url": "/Puzzle/static/media/SlidingCroped.8d3a9594.mp3"
+  },
+  {
+    "revision": "8f980984852521104f62ec11131f12bd",
+    "url": "/Puzzle/static/media/kerber_character.8f980984.png"
+  },
+  {
+    "revision": "464da4b979b863d5730f7fd6e7bd8bed",
+    "url": "/Puzzle/index.html"
+  }
+];
